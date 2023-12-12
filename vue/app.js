6a80124189175ab0dc9b9f1d78086424681fe9f2
laravelkid <nguyenanhkhoa02003@gmail.com>
2023-12-12 22:47:19 +0700

    Learn_01

---
+++ vue/app.js
@@ -0,0 +1,13 @@
+const buttonAddGoal = document.querySelector("button");
+const inputNewGoalString = document.querySelector("input");
+const listNewGoalString = document.querySelector("ul");
+
+function addNewGoalString() {
+  const enteredNewGoalString = inputNewGoalString.value;
+  const listItemNewGoalString = document.createElement("li");
+  listItemNewGoalString.textContent = enteredNewGoalString;
+  listNewGoalString.appendChild(listItemNewGoalString);
+  inputNewGoalString.value = "";
+}
+
+buttonAddGoal.addEventListener("click", addNewGoalString);
